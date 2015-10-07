85dcba343f4f5225e773f70bd03f6006305d124b
emilycw <emily.rose.cw@gmail.com>
2015-10-07 19:14:20 +1000

    write display world

--- src/Main.scala
+++ src/Main.scala
@@ -11,17 +11,35 @@
 class World(cells: Array[Array[Cell]]) {
   var world: Array[Array[Cell]] = cells
 
+  def transition(): World = {
+    this
+  }
+
+  def worldToString(): String = {
+    "---"
+  }
 }
 
-class Cell(xc: Int, yc: Int) {
+class Cell(xc: Int, yc: Int, ty: Char) {
   var xPos: Int = xc
   var yPos: Int = yc
-
+  var thing: Char = ty
 }
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    println("hello world!")
+  def stringToWorld(string: String) = {
+    World([[Cell(1,1,1), Cell(1,1,1)],[Cell(1,1,1), Cell(1,1,1)]])
   }
 
+  def main(args: Array[String]): Unit = {
+    displayWorld(stringToWorld("_--=_ \n ___-_ \n ___-_ \n _---_"))
+  }
+
+  def displayWorld(world: World, upperLimit: Int): World = if (finished(world) || upperLimit <= 0){
+    val newWorld = world.transition()
+    println(worldToString(newWorld))
+    displayWorld(newWorld, upperLimit - 1)
+  } else {
+    world
+  }
 }
